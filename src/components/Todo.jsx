ac0a29afa6cb90943450d40c86f387c3dee08130
Nathalia Dias <158405216+ndiasg@users.noreply.github.com>
2026-01-01 23:28:53 -0300

    feat: deleting todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task, toggleComplete}) => { //recebe a tarefa como prop
+export const Todo = ({task, toggleComplete, deleteTodo}) => { //recebe a tarefa como prop
   return (
     <div className='Todo'>
       <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ''}`}>{task.task}</p> {/* exibe a tarefa e permite marcar como concluída */}
       <div>
         <FontAwesomeIcon icon={faPenToSquare} /> {/* ícone de editar */}
-        <FontAwesomeIcon icon={faTrash} />{/* ícone de deletar */}
+        <FontAwesomeIcon icon={faTrash}  onClick={() => deleteTodo(task.id)}/>{/* ícone de deletar */}
       </div>
     </div>
   )
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -13,13 +13,16 @@ export const TodoWrapper = () => {
   const toggleComplete = id => {
     setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo)) //alterna o status de conclusão da tarefa utilizando o id para identificar a tarefa correta e atualiza o estado
   }
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id)) //remove a tarefa do estado utilizando o id para identificar a tarefa correta
+  }
   return (
     <div className='TodoWrapper'>
       <h1>✨Lista de Tarefas✨</h1>
       <TodoForm addTodo={addTodo /*passa a função addTodo como prop */} />
       {todos.map((todo, index) => ( /*mapeia o array de tarefas*/
         <Todo task={todo} key={index}
-          toggleComplete={toggleComplete} /> /* passa a tarefa como prop */
+          toggleComplete={toggleComplete} deleteTodo={deleteTodo} /> /* passa a tarefa como prop */
       ))}
     </div>
   )
