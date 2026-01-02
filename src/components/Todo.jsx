d28f7b117cfc1b1545bc3c38f96b45db01fe4ad7
Nathalia Dias <158405216+ndiasg@users.noreply.github.com>
2026-01-02 01:46:56 -0300

    feat: add a checkbox for completed tasks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faSquare, faSquareCheck } from '@fortawesome/free-regular-svg-icons'
 
 export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => { //recebe a tarefa como prop
+
   return (
     <div className='Todo'>
+      <FontAwesomeIcon
+        icon={task.completed ? faSquareCheck : faSquare}
+        onClick={() => toggleComplete(task.id)}
+        className='completed'
+      />
       <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ''}`}>{task.task}</p> {/* exibe a tarefa e permite marcar como concluída */}
+      
       <div>
         <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)}/> {/* ícone de editar */}
         <FontAwesomeIcon icon={faTrash}  onClick={() => deleteTodo(task.id)}/> {/*icone de deletar com função de deletar a tarefa*/}
